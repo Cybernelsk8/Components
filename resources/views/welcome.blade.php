c73f529f17f709342e967f8d24791e1e033ffd09
Nelson Ovidio Vasquez Ventura <nelson.o.vasquez@gmail.com>
2022-06-22 11:00:48 -0600

    se mejoro los tooltips

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -280,11 +280,6 @@
                 <x-slot name="msgtooltip">
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo debitis sint optio suscipit, eaque provident totam, sed doloribus nisi voluptatibus odit esse itaque magni! Amet, in! Unde quae provident quas!</p>
                 </x-slot>
-                <x-slot name="icon">
-                    {{-- <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
-                    </svg> --}}
-                </x-slot>
                 <p>Aqui esta el tooltip coloca tu cursor encima</p>
             </x-tooltip>
 
